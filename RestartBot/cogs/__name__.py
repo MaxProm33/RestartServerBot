e5f61f0674e87d199410a89846e032d5dfe918b5
maxprom33 <maxprom33@yandex.ru>
2025-01-07 14:33:09 +0300

    add file

---
+++ RestartBot/cogs/__name__.py
@@ -0,0 +1,4 @@
+from restart import RestartServerSs14Cog
+
+def setup(bot):
+    bot.add_cog(RestartServerSs14Cog(bot))
--- RestartBot/cogs/restart.py
+++ RestartBot/cogs/restart.py
@@ -1,44 +1,59 @@
-from discord import Option
-import discord
-import requests
-from dotenv import load_dotenv
 import os
 import logging
 
-from logger import configure_logging
+import discord
+from discord.ext import commands
+from discord import Option
+import requests
+from dotenv import load_dotenv
+from cogs.ss14_admin_manager.logger import configure_logging
 
 configure_logging(level=logging.INFO)                                               # Подключение логгера, по умолчанию установлен уровень info
-
 load_dotenv()                                                                       # Загрузка .env файла
 
-headers = {                                                                         # Набор данных. Токен администратора находится в .env файле
-    "Authorization": f"SS14Token {os.getenv("SS_TOKEN")}",
-    "Content-Type": "application/json"
+SS_TOKEN = os.getenv("SS_TOKEN")                                                    # Получение токена администратора из .env файл
+
+URLS = {                                                                            # Словарь c url серверов
+    "Титан": "https://ff.deadspace14.net/admin/actions/round/restartnow",
+    "Фобос": "https://ff.deadspace14.net/admin/actions/round/restartnow",
+    "Фронтир": "https://ff.deadspace14.net/admin/actions/round/restartnow",
+    "Апофис": "https://ff.deadspace14.net/admin/actions/round/restartnow",
+    "Деймос": "https://ff.deadspace14.net/admin/actions/round/restartnow",
 }
 
-@discord.slash_command()
-async def restartserver(ctx:discord.ApplicationContext,
-                        server:
-                        Option(str, description="Выберите сервер, который хотите перезагрузить", choices = ["Титан", "Фобос", "Фронтир", "Апофис", "Деймос"])):       # выдача списка серверов пользователю
-    response = ""
+class RestartServerSs14Cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    urls = {                                                                        # Словарь c url серверов
-        "Титан" : "https://ff.deadspace14.net/admin/actions/round/restartnow",
-        "Фобос": "https://ff.deadspace14.net/admin/actions/round/restartnow",
-        "Фронтир": "https://ff.deadspace14.net/admin/actions/round/restartnow",
-        "Апофис": "https://ff.deadspace14.net/admin/actions/round/restartnow",
-        "Деймос": "https://ff.deadspace14.net/admin/actions/round/restartnow",
-    }
+    def restart_server(self, server_name: str) -> requests.Response | None:             # Функция для перезагрузки сервера
+        url = URLS.get(server_name)
+        headers = {
+            "Authorization": f"SS14Token {SS_TOKEN}",
+            "Content-Type": "application/json"
+        }
+        try:
+            response = requests.post(url, headers=headers, timeout=10)                  # Добавляем таймаут
+            response.raise_for_status()                                                 # Вызывает исключение для HTTP ошибок (4xx, 5xx)
+            return response
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Ошибка при перезапуске сервера {server_name}: {e}")
+            return None
 
-    response = requests.post(urls[server], headers=headers)
+    @discord.slash_command(name="restartserver", description="Перезагружает указанный сервер.")# Команда для перезагрузки сервера
+    async def restart_server_command(self, ctx: discord.ApplicationContext,
+                                     server: Option(str, description="Выберите сервер для перезагрузки.",
+                                                   choices=list(URLS.keys()))):
 
-    # Обработка ответа
-    if response.status_code == 200:                                             # Проверка ответа от сервера
-        await ctx.respond(f"{server} успешно перезапущен.")
-        logging.info(f"{server} был успешно перезапущен")
-    else:
-       await ctx.respond(f"К сожалалению не удалось перезапустить {server}. Ошибка: {response.status_code} - {response.text}")
-       logging.info(f"К сожалалению не удалось перезапустить {server}. Ошибка: {response.status_code} - {response.text}")
+        await ctx.defer()                                                               # Откладываем ответ для возможности задержки со стороны сервера
 
-def setup(bot: discord.Bot):                                                    # Загрузка кога
-    bot.add_application_command(restartserver)
+        result = self.restart_server(server)
+
+        if result:
+            await ctx.respond(f"Сервер {server} успешно перезапущен.")
+            logging.info(f"Сервер {server} был успешно перезапущен.")
+        else:
+            await ctx.respond(f"Не удалось перезапустить сервер {server}. "
+                              f"Пожалуйста, обратитесь к администратору.")              # При неудаче логгирование происходит в функции
+
+def setup(bot):                                                                         # Загрузка кога
+    bot.add_cog(RestartServerSs14Cog(bot))
