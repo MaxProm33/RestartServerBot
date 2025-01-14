eb164da026d6ceb9bb2e64268f4476686c990003
maxprom33 <maxprom33@yandex.ru>
2025-01-14 21:05:08 +0300

    Полностью функционирующий ког, работает для всех серверов

--- restart.py
+++ restart.py
@@ -11,18 +11,6 @@ from utils.config_loader import load_online_bots_config
 
 SS_TOKEN = os.getenv("SS_TOKEN")                                                    # Получение токена администратора из .env файл
 
-URLS = {                                                                            # Словарь c url серверов
-    "Титан": "https://ff.deadspace14.net/admin/actions/round/restartnow",
-    "Фобос": "https://ff.deadspace14.net/admin/actions/round/restartnow",
-    "Фронтир": "https://ff.deadspace14.net/admin/actions/round/restartnow",
-    "Апофис": "https://ff.deadspace14.net/admin/actions/round/restartnow",
-    "Деймос": "https://ff.deadspace14.net/admin/actions/round/restartnow",
-    "Союз-1": "https://ff.deadspace14.net/admin/actions/round/restartnow",
-    "Мапперский": "http://185.97.255.17:1225/admin/actions/round/restartnow"
-
-}
-
-
 SERVER_NAME_MAPPING = {                                                              # Словарь соответствий русских и английских названий серверов
     "Титан": "Titan",
     "Фобос": "Fobos",
@@ -36,18 +24,27 @@ class RestartServerSs14Cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.servers = load_online_bots_config("online_servers_bots.json")
-        self.server_tokens = {server.name: server.admin_token for server in self.servers} # создание словаря с данными из json файла
-        print(self.servers[0].admin_token)
-
+        self.server_tokens = {server.name: server.admin_token for server in self.servers} # Создание словаря с данными из json файла по типу: "имя сервера: админ токен"
+        self.server_ips = {server.name: server.ip for server in self.servers}             # Создание словаря с данными из json файла по типу: "имя сервера: ip сервера"
 
     def restart_server(self, server_name: str, ctx: discord.ApplicationContext) -> requests.Response | None:             # Функция для перезагрузки сервера
-        url = URLS.get(server_name)
         english_server_name = SERVER_NAME_MAPPING.get(server_name)
-        admin_token = self.server_tokens.get(english_server_name)                       # Получение админ токена для каждого сервера
-        if not admin_token:
-            logging.error(f"Токен для сервера {server_name} не найден")
+
+        ip_port = self.server_ips.get(english_server_name)
+
+        if not ip_port:
+            self.logger.error(f"IP:порт для сервера {server_name} не найден")
             return None
 
+        url = f"http://{ip_port}/admin/actions/round/restartnow"                        # ip : порт из json файла
+
+        admin_token = self.server_tokens.get(english_server_name)
+
+        if not admin_token:
+            self.logger.error(f"Токен для сервера {server_name} не найден")
+            return None
+
+        admin_token = self.server_tokens.get(english_server_name)                       # Получение админ токена для каждого сервера
 
         try:
             actor_guid = str(uuid.UUID(int=ctx.author.id))
@@ -73,7 +70,7 @@ class RestartServerSs14Cog(commands.Cog):
     @discord.slash_command(name="restartserver", description="Перезагружает указанный сервер.")# Команда для перезагрузки сервера
     async def restart_server_command(self, ctx: discord.ApplicationContext,
                                      server: Option(str, description="Выберите сервер для перезагрузки.",
-                                                   choices=list(URLS.keys()))):
+                                                   choices=list(SERVER_NAME_MAPPING.keys()))):
 
         await ctx.defer()                                                               # Откладываем ответ для возможности задержки со стороны сервера
 
@@ -81,7 +78,7 @@ class RestartServerSs14Cog(commands.Cog):
 
         if result:
             await ctx.respond(f"Сервер {server} успешно перезапущен.")
-            logging.info(f"Сервер {server} был успешно перезапущен.")
+            logging.info(f"Сервер {server} был успешно перезапущен пользователем {ctx.author.name}.")
         else:
             await ctx.respond(f"Не удалось перезапустить сервер {server}. "
                               f"Пожалуйста, обратитесь к администратору.")              # При неудаче логгирование происходит в функции
